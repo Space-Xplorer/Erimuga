d0a1317b5db3eea0fa713e10ae1112b28d7999ff
SpoorthyBoga <spoorthymaharaju@gmail.com>
2025-08-01 18:28:11 +0530

    changes

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -3,13 +3,14 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const productSchema = new Schema({
+  productCode: { type: String, required: true, unique: true }, // <- NEW FIELD
   name: { type: String, required: true },
   mainCategory: { type: String, required: true },
   apparelType: { type: String, required: true },
   subcategory: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: [String], required: true }, // Store local image paths
+  image: { type: [String], required: true },
   isBestSeller: { type: String, enum: ['yes', 'no'], default: 'no' },
   date: { type: Date, default: Date.now }
 });
