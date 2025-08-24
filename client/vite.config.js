31bf39f50d2a6c2001ee458e11fddfea6a1ae892
SpoorthyBoga <spoorthymaharaju@gmail.com>
2025-08-24 22:04:13 +0530

    changes in vit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.PORT) || 5173, // 👈 important for Render
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.PORT) || 4173, // for `vite preview`
+    },
+  }
 })
